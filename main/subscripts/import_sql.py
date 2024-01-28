0cdeeadea739b8b18bf8fbcbfdce9b73d0337a7f
Viktors-K <49311712+Viktors-K@users.noreply.github.com>
2024-01-28 16:46:11 +0200

    Added duplicate check to import_sql

--- main/subscripts/import_sql.py
+++ main/subscripts/import_sql.py
@@ -4,19 +4,19 @@ from csv import reader
 # Importē 'sqlite3' bibleotēkas 'connect' funkciju, kas ļauj savienoties ar .db failu.
 from sqlite3 import connect
 
-# Importē 'os' bibleotēkas 'path' funkciju, kas ļauj formatēt direktorijas.
-# from os import path
-
-# Importē 'import_data' faila 'import_data' funkciju, kas importē datus no dota .csv faila nosaukuma un izvada tos sarakstā.
-from import_data import import_data
-
 # Definē funkciju 'create_db_from_csv' ar diviem ievadiem, 'csv_file', kas pieņem string mainīgo sākuma datu faila nosaukumam un 'db_file', kas pieņem string mainīgo beigu DB faila nosaukumam.
 def create_db_from_csv(csv_file, db_file):
+    
+    # Definē funkciju 'row_exists' ar diviem ievadiem, 'cursor', kas pieņem cursor objektu un 'row', kas pieņem sarakstu, lai pārbaudītu vai tāda rinda darba virsmā jau ir pievienots.
+    def row_exists(cursor, row):
+        cursor.execute("SELECT EXISTS(SELECT 1 FROM main_table WHERE " + " AND ".join(f"{header[i]} = ?" for i in range(len(header))) + ")", row)
+        return cursor.fetchone()[0]
     
     # Izsauc  funkciju 'import_data' un ievada tās izvadu sarakstā 'csv_data'.
     imported_data = import_data(csv_file, 'relational')
     header = imported_data[0]
     csv_data = imported_data[1]
+    
     # Izveido 'conn' mainīgo ar SQLite3 savienojumu failam 'db_file'.
     conn = connect(db_file)
     
@@ -26,10 +26,11 @@ def create_db_from_csv(csv_file, db_file):
     # Izsauc SQL komandu lai izveidotu jaunu darba virmsu 'main_table' .db failā un pievieno pirmo rindu no .csv faila kā galveni darba virsmai.
     cursor.execute(f"CREATE TABLE IF NOT EXISTS main_table ({', '.join(header)})")
     
-    # Pievieno katru rindu no .csv faila 'main_table' darba virsmā.
+    # Pievieno katru rindu no .csv faila 'main_table' darba virsmā, ja tā nav jau pievienota.
     for row in csv_data:
-        placeholders = ', '.join(['?' for _ in row])
-        cursor.execute(f"INSERT INTO main_table VALUES ({placeholders})", row)
+        if not row_exists(cursor, row):
+            placeholders = ', '.join(['?' for _ in row])
+            cursor.execute(f"INSERT INTO main_table VALUES ({placeholders})", row)
 
     # Izmantojot 'commit' funkciju, izmaiņas tiek apstiprinātas un ar 'close' funkciju, .db fails tiek aizvērts.
     conn.commit()
@@ -42,5 +43,10 @@ def main():
 
 # Ja atvērts tieši, šis skripts izpildīs main() funckiju.
 if __name__ == "__main__":
+    # Importē 'import_data' faila 'import_data' funkciju, kas importē datus no dota .csv faila nosaukuma un izvada tos sarakstā.
+    from import_data import import_data
     main()
+else:
+    # Importē 'import_data' faila 'import_data' funkciju, kas importē datus no dota .csv faila nosaukuma un izvada tos sarakstā.
+    from subscripts.import_data import import_data
 
